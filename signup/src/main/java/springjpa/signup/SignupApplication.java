0b960bf6bace8031231a9eb9c0ad429e3b3e7966
kjungw1025 <kjungw1025@naver.com>
2023-08-22 22:50:09 +0900

    conf: Settings project

---
+++ signup/src/main/java/springjpa/signup/SignupApplication.java
@@ -0,0 +1,13 @@
+package springjpa.signup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignupApplication.class, args);
+	}
+
+}
